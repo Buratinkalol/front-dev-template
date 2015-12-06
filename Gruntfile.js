b3d7869431cdc1d8487ff580b2dfdd363b083266
Buratinkalol <h12vbn6@gmail.com>
2015-12-07 00:18:21 +0300

    Angular, Bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function (grunt) {
                     'src/**/*.css'
                 ],
                 options: {
-                    iterrupt: true,
+                    interrupt: true,
                     livereload: true
                 }
             }
